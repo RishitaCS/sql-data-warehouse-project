c33bde64501f19ea01c775e70477ad120b3c5b2d
Rishita <154823062+RishitaCS@users.noreply.github.com>
2026-05-30 21:53:39 +0530

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -13,14 +13,14 @@ IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
     DROP TABLE bronze.crm_cust_info;
 GO
 
-Create Table bronze.crm_cust_info(
-	cst_id Int,
-	cst_key NVarchar(50),
-	cst_firstname NVarchar(50),
-	cst_lastname NVarchar(50),
-	cst_material_status NVarchar(50),
-	cst_gndr NVarchar(50),
-	cst_create_date DATE
+CREATE TABLE bronze.crm_cust_info (
+    cst_id              INT,
+    cst_key             NVARCHAR(50),
+    cst_firstname       NVARCHAR(50),
+    cst_lastname        NVARCHAR(50),
+    cst_marital_status  NVARCHAR(50),
+    cst_gndr            NVARCHAR(50),
+    cst_create_date     DATE
 );
 GO
 
@@ -28,14 +28,14 @@ IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
     DROP TABLE bronze.crm_prd_info;
 GO
 
-Create Table bronze.crm_prd_info(
-	prd_id Int,
-	prd_key NVarchar(50),
-	prd_nm NVarchar(50),
-	prd_cost Int,
-	prd_line NVarchar(50),
-	prd_start_dt DATETIME,
-	prd_end_dt DATETIME
+CREATE TABLE bronze.crm_prd_info (
+    prd_id       INT,
+    prd_key      NVARCHAR(50),
+    prd_nm       NVARCHAR(50),
+    prd_cost     INT,
+    prd_line     NVARCHAR(50),
+    prd_start_dt DATETIME,
+    prd_end_dt   DATETIME
 );
 GO
 
@@ -43,16 +43,16 @@ IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
     DROP TABLE bronze.crm_sales_details;
 GO
 
-Create Table bronze.crm_sales_details(
-	sls_ord_num NVarchar(50),
-	sls_prd_key NVarchar(50),
-	sls_cust_id INT,
-	sls_order_dt INT,
-	sls_ship_dt INT,
-	sls_due_dt INT,
-	sls_sales INT,
-	sls_quantity INT,
-	sls_price INT
+CREATE TABLE bronze.crm_sales_details (
+    sls_ord_num  NVARCHAR(50),
+    sls_prd_key  NVARCHAR(50),
+    sls_cust_id  INT,
+    sls_order_dt INT,
+    sls_ship_dt  INT,
+    sls_due_dt   INT,
+    sls_sales    INT,
+    sls_quantity INT,
+    sls_price    INT
 );
 GO
 
@@ -60,9 +60,9 @@ IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
     DROP TABLE bronze.erp_loc_a101;
 GO
 
-Create Table bronze.erp_loc_a101(
-	cid NVarchar(50),
-	cntry NVarchar(50)
+CREATE TABLE bronze.erp_loc_a101 (
+    cid    NVARCHAR(50),
+    cntry  NVARCHAR(50)
 );
 GO
 
@@ -70,10 +70,10 @@ IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
     DROP TABLE bronze.erp_cust_az12;
 GO
 
-Create Table bronze.erp_cust_az12(
-	cid NVarchar(50),
-	bdate DATE,
-	gen NVarchar(50)
+CREATE TABLE bronze.erp_cust_az12 (
+    cid    NVARCHAR(50),
+    bdate  DATE,
+    gen    NVARCHAR(50)
 );
 GO
 
@@ -81,10 +81,10 @@ IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
     DROP TABLE bronze.erp_px_cat_g1v2;
 GO
 
-Create Table bronze.erp_px_cat_g1v2(
-	id NVarchar(50),
-	cat NVarchar(50),
-	subcat NVarchar(50),
-	maintenance NVarchar(50)
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+    id           NVARCHAR(50),
+    cat          NVARCHAR(50),
+    subcat       NVARCHAR(50),
+    maintenance  NVARCHAR(50)
 );
 GO
